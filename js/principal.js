80f221ee7f30b976c8254775766fec2116be6c8d
Your Namegit <you@example.com>
2021-08-19 11:35:02 -0300

    calculating imc using logical operators

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,14 @@
-console.log("Ola mundo")
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+tdImc = paciente.querySelector(".info-imc");
+var imc = peso / (altura * altura);
+tdImc.textContent = imc;
